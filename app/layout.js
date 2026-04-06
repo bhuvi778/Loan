914c4edf72ac97a4eeeef4069f67d806febbb827
bhuvi778 <bhuvi778@github.com>
2026-04-07 00:29:28 +0530

    Revert "fix: replace testimonial photos with verified Indian professional faces from Pexels"
    This reverts commit 6421dcfee6729ca42de8d1a0614dbd0643371eca.

--- app/layout.js
+++ app/layout.js
@@ -5,8 +5,6 @@ const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700", "800", "900"],
-  display: "swap",
-  preload: false,
 });
 
 export const metadata = {
